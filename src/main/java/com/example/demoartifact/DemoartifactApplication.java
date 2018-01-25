ecbf5d6de24078f95c071d162ee7c21d28f9f23b
Brandon Hammiller <bdayton1985@gmail.com>
2018-01-25 09:15:57 -0500

    Initial commit

---
+++ src/main/java/com/example/demoartifact/DemoartifactApplication.java
@@ -0,0 +1,12 @@
+package com.example.demoartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoartifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoartifactApplication.class, args);
+	}
+}
